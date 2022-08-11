26e2b46e58117cf82f5fde81e9987febcff00fe1
kia1016 <kia1016@gmail.com>
2022-08-10 23:21:13 -0400

    React-Intro

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <header>This is my first React App!</header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Yay for React! New module!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
